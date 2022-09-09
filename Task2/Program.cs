94a2c11147a2e5c6780bfc250b1cf8fedbd2a88a
ndzd <n_level@mail.ru>
2022-09-09 20:38:35 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int GetSum(int number){
+    int a = 0;
+    int b = 0;
+    int num = number;
+
+    while(num / 10 > 0){
+        a = a + (num % 10);
+        num = num / 10;
+    }
+    b = a + num;
+
+    return b;
+}
+System.Console.WriteLine(GetSum(9012));
